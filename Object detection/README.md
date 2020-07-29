902ad4bcdf00b5bbc9c841982b72ecabb90e6fd2
luckyCasualGuy <luckyCasualGuy@protonmail.com>
2020-07-29 21:59:00 +0530

    Added make_DB.py

--- Object detection/README.md
+++ Object detection/README.md
@@ -1,1 +1,30 @@
 # All the Scripts That I used in Object detection are HERE!!
+
+I used **`https://github.com/AlexeyAB/darknet`** for custom object detection.  
+All scripts in this DIR are made to make my life easy using AlexeyAB's library!  
+
+<hr>
+
+**make_DB.py**: This script is used to make Database out of Images.  
+<br>
+**This is how you use it:**  
+`python make_DB.py -s <source> -d <destination>`
+<br>
+source should be like:  
+>source  
+>>class1
+>>>classes.txt
+>>>img1.jpg
+>>>img1.txt
+>>> & so on...
+>>class2
+>> & so on...
+
+output will be like:  
+>destination
+>>classes.txt
+>>img1.jpg
+>>img1.txt
+>> & so on...
+
+<hr>
---
+++ Object detection/make_DB.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from shutil import copyfile
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--src', type=str, required=True, help="Source path")
+parser.add_argument('-d', '--dst', type=str, required=True, help="Destination path")
+args = parser.parse_args()
+
+final = Path(args.dst)
+p = Path(args.src)
+
+img_dirs = [x for x in p.iterdir() if p.is_dir()]
+classes = []
+
+for i, dirs in enumerate(img_dirs, 0):
+    
+    clss = dirs / 'classes.txt'
+    classes.append(clss.read_text())
+    
+    imgs = sorted(dirs.glob("[!classes]*.txt")) 
+    for img in imgs: img.write_text(img.read_text().replace('0 ', str(i)+' '))
+        
+    imgs = sorted(dirs.glob("[!classes]*.*"))
+    for img in imgs: copyfile(img, final / img.parts[-1])
+
+clss = final / 'classes.txt'
+write_str = ''.join(classes)
+with clss.open(mode='w+') as f:
+    f.write(write_str)
+    f.close()
+
